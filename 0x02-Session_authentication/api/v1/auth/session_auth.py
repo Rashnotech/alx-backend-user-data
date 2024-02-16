4c063d68e89be022810fb4be4ac08a836207965c
Rashnotech <rashnotech@gmail.com>
2024-02-16 23:09:16 +0000

    Create a session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 """a module Session Auth"""
 from .auth import Auth
+import uuid
 
 
 class SessionAuth(Auth):
     """A Session Authentication Class"""
-    pass
+    user_id_by_session_id = {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """a method that create session"""
+        if user_id and isinstance(user_id, str):
+            sess_id = uuid.uuid4()
+            self.user_id_by_session_id[sess_id] = user_id
+            return sess_id
+        return None

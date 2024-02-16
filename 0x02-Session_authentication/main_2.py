8bdbef826ae320ea560a56b4c61f112110d9709d
Rashnotech <rashnotech@gmail.com>
2024-02-16 23:15:23 +0000

    User ID for Session ID

---
+++ 0x02-Session_authentication/main_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Main 2
+"""
+from api.v1.auth.session_auth import SessionAuth
+
+sa = SessionAuth()
+
+user_id_1 = "abcde"
+session_1 = sa.create_session(user_id_1)
+print("{} => {}: {}".format(user_id_1, session_1, sa.user_id_by_session_id))
+
+user_id_2 = "fghij"
+session_2 = sa.create_session(user_id_2)
+print("{} => {}: {}".format(user_id_2, session_2, sa.user_id_by_session_id))
+
+print("---")
+
+tmp_session_id = None
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+tmp_session_id = 89
+tmp_user_id = sa.user_id_for_session_id(tmp_session_id)
+print("{} => {}".format(tmp_session_id, tmp_user_id))
+
+tmp_session_id = "doesntexist"
+

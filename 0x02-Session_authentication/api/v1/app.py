5e3dc0af76c53f0389596cbcd12d66916d662cb4
Rashnotech <rashnotech@gmail.com>
2024-02-16 22:50:50 +0000

    Empty session

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -8,6 +8,7 @@ from flask import Flask, jsonify, request, abort
 from flask_cors import CORS
 from api.v1.auth.basic_auth import BasicAuth
 from api.v1.auth.auth import Auth
+from api.v1.auth.session_auth import SessionAuth
 
 
 app = Flask(__name__)
@@ -19,6 +20,8 @@ app.register_blueprint(app_views)
 
 if auth_type == 'basic_auth':
     auth = BasicAuth()
+elif auth_type == 'session_auth':
+    auth = SessionAuth()
 else:
     auth = Auth()
 
---
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""a module Session Auth"""
+from .auth import Auth
+
+
+class SessionAuth(Auth):
+    """A Session Authentication Class"""
+    pass

3691eb1107c159cefabf0952cd0b63c6d9e74d8d
Rashnotech <rashnotech@gmail.com>
2024-02-20 14:21:13 +0000

    Expiration session

--- 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_exp_auth.py
@@ -28,27 +28,27 @@ class SessionExpAuth(SessionAuth):
     def create_session(self, user_id=None) -> str:
         """create session"""
         session_id = super().create_session(user_id)
-        if session_id is None:
+        if not session_id:
             return None
         session_dict = {
                 'user_id': user_id,
-                'created_at': datetime.now()
-        }
-        SessionAuth.user_id_by_session_id[session_id] = session_dict
+                'created_at': datetime.now()}
+        self.user_id_by_session_id[session_id] = session_dict
         return session_id
 
     def user_id_for_session_id(self, session_id=None):
         """user id for session id"""
-        if session_id is None:
+        if not session_id:
             return None
         session_dict = self.user_id_session_id.get(session_id)
-        if session_dict is None:
+        if not session_dict:
             return None
         user_id = session_dict.get('user_id')
         created_at = session_dict.get('created_at')
 
         if self.session_duration <= 0:
             return user_id
-        if created_at is None or (created_at + timedelta(seconds=self.session_duration)):
-            return None
-        return user_id
+        if created_at:
+            if created_at + timedelta(seconds=self.session_duration)):
+                return user_id
+        return None

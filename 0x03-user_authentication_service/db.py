238b9e68a2ab71b5698b5578446d62980843150e
Rashnotech <rashnotech@gmail.com>
2024-02-21 22:18:46 +0000

    Fixed: annotate class

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -29,10 +29,9 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email, hashed_password):
+    def add_user(self, email: str, hashed_password: str) -> User:
         """a method that add user"""
         user = User(email=email, hashed_password=hashed_password)
         self._session.add(user)
         self._session.commit()
         return user
-

b89a23e1c1e686d56e0051206c010477e72b3214
Rashnotech <rashnotech@gmail.com>
2024-02-21 16:33:50 +0000

    Fixed: name error

--- 0x02-Session_authentication/models/user_session.py
+++ 0x02-Session_authentication/models/user_session.py
@@ -10,6 +10,6 @@ class UserSession(Base):
 
     def __init__(self, *arg: list, **kwargs: dict):
         """Initialization"""
-        super().__init__(*args, **kwargs)
+        super().__init__(*args)
         self.user_id = kwargs.get('user_id')
         self.session_id = kwargs.get('session_id')

38c0ba3d09e296f449f120cf48ffffbf5e558ca7
Rashnotech <rashnotech@gmail.com>
2024-02-23 15:22:18 +0000

    Credential validation

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -3,6 +3,7 @@
 import bcrypt
 from db import DB
 from sqlalchemy.orm.exc import NoResultFound
+from sqlalchemy.exc import InvalidRequestError
 from user import User
 
 
@@ -18,7 +19,6 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-
     def register_user(self, email: str, password: str) -> User:
         """a method that register users"""
         try:
@@ -29,3 +29,13 @@ class Auth:
             pwd = _hash_password(password)
             user = self._db.add_user(email, pwd)
             return user
+
+    def valid_login(self, email: str, password: str) -> bool:
+        """a method that validate credentials"""
+        try:
+            user = self._db.find_user_by(email=email)
+            pwd = user.hashed_password
+            return bcrypt.checkpw(password.encode('utf-8'), pwd)
+        except (InvalidRequestError, NoResultFound):
+            return False
+        return False

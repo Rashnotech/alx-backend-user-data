0bb2d789a29ce9671466c56d712231d594711af2
Rashnotech <rashnotech@gmail.com>
2024-02-23 22:30:51 +0000

    Fixed: passed dict

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -62,7 +62,7 @@ class Auth:
         if session_id is None:
             return None
         try:
-            user = self._db.find_user_by(session_id)
+            user = self._db.find_user_by(session_id=session_id)
+            return user
         except (InvalidRequestError, NoResultFound):
             return None
-        return user

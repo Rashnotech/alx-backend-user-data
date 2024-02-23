d6f7364aa55c176a4df25aad34cc364d6ab79a0f
Rashnotech <rashnotech@gmail.com>
2024-02-23 04:42:58 +0000

    Flask app entry

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -1,8 +1,31 @@
 #!/usr/bin/env python3
 """a module that handle authentication"""
 import bcrypt
+from db import DB
+from sqlalchemy.orm.exc import NoResultFound
+from user import User
 
 
 def _hash_password(password):
     """a method that handles hashing of password"""
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+
+
+class Auth:
+    """Auth class to interact with the authentication database.
+    """
+
+    def __init__(self):
+        self._db = DB()
+
+
+    def register_user(self, email: str, password: str) -> User:
+        """a method that register users"""
+        try:
+            user = self._db.find_user_by(email=email)
+            if user:
+                raise ValueError(f"<user's {email}> already exists")
+        except NoResultFound:
+            pwd = _hash_password(password)
+            user = self._db.add_user(email, pwd)
+            return user
